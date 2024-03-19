8e56d07ddf0b30f00341502fe48d13639d9d5334
WilliamMRS <schmidtwilliammikal@gmail.com>
2024-03-19 18:47:39 +0100

    basic ui

--- AI_System/ui/public/index.js
+++ AI_System/ui/public/index.js
@@ -1,6 +1,32 @@
+messageLog = []
 
 init = () => {
     console.log("Hello from the client side!")
+    document.getElementById('send_button').addEventListener('click', sendMessage)
 }
+
+sendMessage = async () => {
+    const user_input = document.getElementById('chat_input_text').value
+    document.getElementById('chat_input_text').value = ''
+    res = await fetch('/send_message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: user_input, source: 'user' })
+    })
+    res.json().then(data => {
+        console.log(data)
+        messageLog = data
+        renderMessages()
+    })
+}
+
+renderMessages = () => {
+    const chat_box = document.getElementById('chat_history')
+    allChats = []
+    chat_box.value = messageLog.join('\n')
+}
+
 
 window.onload = init;
--- AI_System/ui/server.js
+++ AI_System/ui/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.json())
 
 app.use(express.static('public'))
 
@@ -8,6 +10,17 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.post('/send_message', (req, res) => {
+    const data = req.body.message
+    console.log(data)
+    messageLog.push(data)
+    res.send(messageLog)
+})
+
+// TODO Setup a websocket connection for streaming results live.
+
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
 })
+
+messageLog = []
