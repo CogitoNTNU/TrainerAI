5e02ace18aa136a890ad8b0d67e44601ef29f4b9
tryggve <tryggvekj@gmail.com>
2024-04-25 15:20:46 +0200

    add support for langsmith

--- AI_System/langchain/config.py
+++ AI_System/langchain/config.py
@@ -6,6 +6,10 @@ load_dotenv(dotenv_path="../.env")
 OPENAI_API_KEY: str = os.getenv(key="OPENAI_API_KEY")
 LANGSMITH_API_KEY: str = os.getenv(key="LANGSMITH_API_KEY")
 
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"] = LANGSMITH_API_KEY
+
 if __name__ == "__main__":
     print(f"[INFO] OPENAI_API_KEY: {OPENAI_API_KEY}")
     print(f"[INFO] LANGSMITH_API_KEY: {LANGSMITH_API_KEY}")
