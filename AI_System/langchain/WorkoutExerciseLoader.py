390e9f2a934cc9583eddcc7572fb5a7c124faded
eldar <eldar.alvik@outlook.com>
2024-04-25 20:50:29 +0200

    fixed tool names

--- AI_System/langchain/WorkoutExerciseLoader.py
+++ AI_System/langchain/WorkoutExerciseLoader.py
@@ -17,7 +17,7 @@ from langchain.tools import BaseTool, StructuredTool, tool
 
 class set_workouts_csv_location_paramaters(BaseModel):
     datapath: str = Field(description="Should be the datapath to workouts.csv CSV-file")
-@tool("set-workouts-csv-location", args_schema=set_workouts_csv_location_paramaters, return_direct=True)
+@tool("set_workouts_csv_location", args_schema=set_workouts_csv_location_paramaters, return_direct=True)
 def set_workouts_csv_location(datapath: str):
     """sets the datapath to workouts.csv"""
     global workouts_csv_location
@@ -25,16 +25,16 @@ def set_workouts_csv_location(datapath: str):
 
 class set_workout_csv_location_paramaters(BaseModel):
     datapath: str = Field(description="Should be the datapath to workout.csv CSV-file")
-@tool("set-workout-csv-location", args_schema=set_workout_csv_location_paramaters, return_direct=True)
+@tool("set_workout_csv_location", args_schema=set_workout_csv_location_paramaters, return_direct=True)
 def set_workout_csv_location(datapath: str):
     """sets the datapath to workout.csv"""
     global workout_csv_location
     workout_csv_location = datapath
 
-class create_workout_csv(BaseModel):
+class create_workout_csv_parameters(BaseModel):
     datapath: str = Field(description="Should be the datapath to workout.csv CSV-file that is set by set-workout-csv-location")
 #mulig å endre denne til å lage en ny csv fil med egenartet navn
-@tool("create-workout-csv", args_schema=create_workout_csv, return_direct=True)
+@tool("create_workout_csv", args_schema=create_workout_csv_parameters, return_direct=True)
 def create_workout_csv(datapath: str):
     """creates a csv file with the columns excercise, sets, reps, vekt, RPE and explenation to save a workout in"""
     workout = pd.DataFrame({"exercise":[],
@@ -51,7 +51,7 @@ def create_workout_csv(datapath: str):
 class add_excercise_to_workout_plan_parameters(BaseModel):
     exercise: str = Field(description="Should be the exercise you want to add to the workout plan, this input should be a string with the name of the excercise from workouts.csv, it row/index should be the same as in the workout.csv file") 
 
-@tool("add-exercise-to-workout-plan",args_schema=add_excercise_to_workout_plan_parameters, return_direct=True)
+@tool("add_excercise_to_workout_plan",args_schema=add_excercise_to_workout_plan_parameters, return_direct=True)
 def add_excercise_to_workout_plan(exercise:str):
     """adds an exercise to the workout plan with the explenation from workouts.csv and sets it to the workout.csv file"""
     workouts = pd.read_csv(workouts_csv_location)  #åpner workouts.csv
@@ -65,7 +65,7 @@ def add_excercise_to_workout_plan(exercise:str):
 
 class set_exercise_paramaters(BaseModel):
     exercise: str = Field(description="Should be the exercise you want to add to the workout plan, this input should be a string with the name of the excercise from workouts.csv, it row/index should be the same as in the workout.csv file")
-@tool("set-exercise", args_schema=set_exercise_paramaters, return_direct=True)
+@tool("set_excercise_to_add", args_schema=set_exercise_paramaters, return_direct=True)
 def set_excercise_to_add(exercise):
     """sets the exercise to add to the workout plan"""
     global set_exercise
@@ -77,7 +77,7 @@ class add_sets_to_excercise_paramaters(BaseModel):
     sets: str = Field(description="Should be the number of sets in the excerise")
 
 set_exercise = "squat"
-@tool("add-sets-to-exercise", args_schema=add_sets_to_excercise_paramaters, return_direct=True)
+@tool("add_sets_to_exercise", args_schema=add_sets_to_excercise_paramaters, return_direct=True)
 def add_sets_to_exercise(sets:str):
     """adds sets to an exercise in the workout plan to the workout.csv file"""
     workout = pd.read_csv(workout_csv_location,index_col="exercise")  #åpner workout.csv
@@ -88,7 +88,7 @@ class add_reps_to_exercise_paramaters(BaseModel):
     #exercise: str = Field(description="Should be the exercise you want to add to the workout plan, this input should be a string with the name of the excercise from workouts.csv, the row/index should be the same as in the workout.csv file")
     reps: str = Field(description="Should be the number of reps in the excerise")
 
-@tool("add-reps-to-exercise", args_schema=add_reps_to_exercise_paramaters, return_direct=True)
+@tool("add_reps_to_exercise", args_schema=add_reps_to_exercise_paramaters, return_direct=True)
 def add_reps_to_exercise(reps):
     """adds reps to an exercise in the workout plan to the workout.csv file"""
     workout = pd.read_csv(workout_csv_location,index_col="exercise")  #åpner workout.csv
@@ -99,7 +99,7 @@ class add_weight_to_exercise_paramaters(BaseModel):
     #exercise: str = Field(description="Should be the exercise you want to add to the workout plan, this input should be a string with the name of the excercise from workouts.csv, the row/index should be the same as in the workout.csv file")
     weight: int = Field(description="Should be the weight used for the excercise")
 
-@tool("add-weight-to-exercise", args_schema=add_weight_to_exercise_paramaters, return_direct=True)
+@tool("add_weight_to_exercise", args_schema=add_weight_to_exercise_paramaters, return_direct=True)
 def add_weight_to_exercise(weight):
     """adds weight to an exercise in the workout plan to the workout.csv file"""
     if int(weight) == 0:
@@ -114,7 +114,7 @@ class add_rest_to_exercise_paramaters(BaseModel):
     #exercise: str = Field(description="Should be the exercise you want to add to the workout plan, this input should be a string with the name of the excercise from workouts.csv, the row/index should be the same as in the workout.csv file")
     rest: int = Field(description="Should be the rest time between sets")
 
-@tool("add-rest-to-exercise", args_schema=add_rest_to_exercise_paramaters, return_direct=True)
+@tool("add_rest_to_exercise", args_schema=add_rest_to_exercise_paramaters, return_direct=True)
 def add_rest_to_exercise(rest):
     """adds rest time time in minutes to an exercise based on the RPE of the excerscie in the workout plan to the workout.csv file"""
     workout = pd.read_csv(workout_csv_location,index_col="exercise")  #åpner workout.csv
@@ -125,7 +125,7 @@ class workout_csv_paramaters(BaseModel):
     #exercise: str = Field(description="Should be the exercise you want to add to the workout plan, this input should be a string with the name of the excercise from workouts.csv, the row/index should be the same as in the workout.csv file")
     RPE: str = Field(description="Should be the RPE for the excercise, should be a number between 1 and 10 to set the precived intensity of the excercise")
 
-@tool("add-RPE-to-exercise", args_schema=workout_csv_paramaters, return_direct=True)
+@tool("add_RPE_to_exercise", args_schema=workout_csv_paramaters, return_direct=True)
 def add_RPE_to_exercise(RPE):
     """adds RPE to an exercise in the workout plan to the workout.csv file"""
     if int(RPE) > 10:
@@ -139,7 +139,7 @@ class add_time_to_exercise_paramaters(BaseModel):
     #exercise: str = Field(description="Should be the exercise you want to add to the workout plan, this input should be a string with the name of the excercise from workouts.csv, the row/index should be the same as in the workout.csv file")
     time: int = Field(description="estimated time to complete reps and sets with estimated rest")
 
-@tool("add-time-to-exercise", args_schema=add_time_to_exercise_paramaters, return_direct=True)
+@tool("add_time_to_exercise", args_schema=add_time_to_exercise_paramaters, return_direct=True)
 def add_time_to_exercise(time):
     """adds time in minutes to an exercise in the workout plan to the workout.csv file"""
     workout = pd.read_csv(workout_csv_location,index_col="exercise")  #åpner workout.csv
@@ -148,9 +148,8 @@ def add_time_to_exercise(time):
 
 
 
-print(add_excercise_to_workout_plan.name)
 
-""" set_workouts_csv_location("AI_System/langchain/workouts.csv")
+ """set_workouts_csv_location("AI_System/langchain/workouts.csv")
 set_workout_csv_location("AI_System/langchain/workout.csv")
 create_workout_csv(workout_csv_location)
 set_excercise_to_add("squat")
