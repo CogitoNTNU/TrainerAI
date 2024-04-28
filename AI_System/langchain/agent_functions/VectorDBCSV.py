cd0ad03b999d4136335c0dcb343931b4b6072675
eldar <eldar.alvik@outlook.com>
2024-04-28 21:15:41 +0200

    vector db search funker for spesifike øvelser men ikke for en generell / tomt søk

--- AI_System/langchain/agent_functions/VectorDBCSV.py
+++ AI_System/langchain/agent_functions/VectorDBCSV.py
@@ -1,4 +1,4 @@
-from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders.csv_loader import CSVLoader
 import os
@@ -20,20 +20,37 @@ def create_exercises_vectorDB():
     os.chdir("./vectorDB")
     exercises.save_local("exercises")
     os.chdir(current_dir)
+    return print("Vector database created")
 
 
 class search_exercises_vectorDB_parameters(BaseModel):
-    search_query: str = Field("The search query is the excersice, equipment, trainingtype etc, muscle group you want to search for in the vector database")
+    search_query: str = Field("The search query is the excersice, equipment, trainingtype etc, muscle group you want to search for in the vector database, the input cant be empty")
 @tool("search_exercises_vectorDB", args_schema=search_exercises_vectorDB_parameters)
-def search_exercises_vectorDB(search_query: str):
-    """A function for searching for exercises in the vector database"""
+def search_exercises_vectorDB(search_query: str = None):
+    """A function for searching  trough the vector db exercise in the vector database"""
     if search_query == None:
-        return search_query == "exercises"
+        return search_query == "a"
     embeddings = OpenAIEmbeddings()
     query = search_query
     current_dir = os.getcwd()
     os.chdir("./vectorDB")
-    new_exercises = FAISS.load_local("exercises",embeddings)
+    new_exercises = FAISS.load_local("exercises",embeddings,allow_dangerous_deserialization=True)
+    results = new_exercises.similarity_search(query,10)
+    os.chdir(current_dir)
+    return results
+
+class search_exercises_vectorDB__parameters(BaseModel):
+    search_query: str = Field("The search query is the excersice, equipment, trainingtype etc, muscle group you want to search for in the vector database, the input cant be empty")
+@tool("search_exercises_vectorDB", args_schema=search_exercises_vectorDB_parameters)
+def search_exercises_vectorDB(search_query: str = None):
+    """A function for searching  trough the vector db exercise in the vector database"""
+    if search_query == None:
+        return search_query == "a"
+    embeddings = OpenAIEmbeddings()
+    query = search_query
+    current_dir = os.getcwd()
+    os.chdir("./vectorDB")
+    new_exercises = FAISS.load_local("exercises",embeddings,allow_dangerous_deserialization=True)
     results = new_exercises.similarity_search(query,10)
     os.chdir(current_dir)
     return results
